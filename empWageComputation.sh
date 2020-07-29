897cbd929c4eae4682f1310a46262521302d8145
Gopi <gopikishanjakkam@gmail.com>
2020-07-29 23:10:26 +0530

    Add Daily Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,11 +2,17 @@
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
-isPresent=1;
+isPresent=1
+empWagePerHr=20
 randomcheck=$(( $RANDOM%2 ));
 if [ $isPresent -eq $randomcheck ];
-   then
-      echo "Employee is present";
-   else
-      echo "Employee is absent";
+then
+    echo "Employee is present";
+    empHrs=8;
+else
+    echo "Employee is absent";
+    empHrs=0;
 fi
+
+salary=$(($empWagePerHr*$empHrs))
+echo "Employees Wage Today="$salary
