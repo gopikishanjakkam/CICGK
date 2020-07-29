3422a3cc992a6e0d66f16fc100bd202c24941600
Gopi <gopikishanjakkam@gmail.com>
2020-07-29 23:50:24 +0530

    Add Part Time Employee and Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,13 +2,18 @@
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
-isPresent=1
+isFullTime=0
+isPartTime=1
 empWagePerHr=20
-randomcheck=$(( $RANDOM%2 ));
-if [ $isPresent -eq $randomcheck ];
+randomcheck=$(( $RANDOM%3 ));
+if [ $isFullTime -eq $randomcheck ];
 then
-    echo "Employee is present";
+    echo "Employee is present for full time";
     empHrs=8;
+elif [ $isPartTime -eq $randomcheck ];
+then
+    echo "Employee is present for Part time";
+    empHrs=4;
 else
     echo "Employee is absent";
     empHrs=0;
