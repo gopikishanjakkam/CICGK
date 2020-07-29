49651e5382a9d13022ee0114fe164cf800377338
Gopi <gopikishanjakkam@gmail.com>
2020-07-29 21:55:59 +0530

    Adding Employee Attendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+isPresent=1;
+randomcheck=$(( $RANDOM%2 ));
+if [ $isPresent -eq $randomcheck ];
+   then
+      echo "Employee is present";
+   else
+      echo "Employee is absent";
+fi
