8b36a80064a5f29a67004954217a0184e30005b5
Gopi <gopikishanjakkam@gmail.com>
2020-08-06 05:27:21 +0530

    Add check employee is present or absent

--- Day19probEmpWage.java
+++ Day19probEmpWage.java
@@ -3,6 +3,14 @@ public class EmployeeWageComputation
 	public static void main(String[] args)
 		{
    	System.out.println("Welcome to employee wage computation program on master branch");
+		int IS_PRESENT=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_PRESENT)
+			{
+				System.out.println("Employee is present");
+			}
+		else
+				System.out.println("Employee is absent");
 		}
 
 }
