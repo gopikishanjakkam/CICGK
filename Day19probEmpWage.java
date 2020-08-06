c099fa292bca20542068b941e8f60979c3f9a584
Gopi <gopikishanjakkam@gmail.com>
2020-08-06 05:49:03 +0530

    Add part time employee and wage calculation

--- Day19probEmpWage.java
+++ Day19probEmpWage.java
@@ -3,14 +3,20 @@ public class EmployeeWageComputation
 	public static void main(String[] args)
 		{
    	System.out.println("Welcome to employee wage computation program on master branch");
-		int IS_PRESENT=1;
-		int WAGE_PER_HR=20;
+		final int IS_FULL_TIME=1;
+		final int IS_PART_TIME=2;
+		final int WAGE_PER_HR=20;
 		int empHrs=0,empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck == IS_PRESENT)
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(empCheck == IS_FULL_TIME)
 			{
-				System.out.println("Employee is present");
+				System.out.println("Employee is present for full time");
 				empHrs=8;
+			}
+		else if(empCheck == IS_PART_TIME)
+			{
+				System.out.println("Employee is present for part time");
+				empHrs=4;
 			}
 		else
 				System.out.println("Employee is absent");
