18534204c6f88505b80bcf27ee4af26855b2a242
Gopi <gopikishanjakkam@gmail.com>
2020-08-06 05:39:57 +0530

    Add daily wage calculation

--- Day19probEmpWage.java
+++ Day19probEmpWage.java
@@ -4,13 +4,18 @@ public class EmployeeWageComputation
 		{
    	System.out.println("Welcome to employee wage computation program on master branch");
 		int IS_PRESENT=1;
+		int WAGE_PER_HR=20;
+		int empHrs=0,empWage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck == IS_PRESENT)
 			{
 				System.out.println("Employee is present");
+				empHrs=8;
 			}
 		else
 				System.out.println("Employee is absent");
+				empWage=empHrs*WAGE_PER_HR;
+				System.out.println("Employee wage="+empWage);
 		}
 
 }
